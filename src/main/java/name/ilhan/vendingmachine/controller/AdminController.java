ab32fb3bcd3c029b30ef9c5018c2ae07ac829c2e
Ilhan Yumer <ilhan@ilhan.name>
2023-07-08 13:27:27 +0300

    Limit shelves with 10

--- src/main/java/name/ilhan/vendingmachine/controller/AdminController.java
+++ src/main/java/name/ilhan/vendingmachine/controller/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -17,8 +18,18 @@ public class AdminController {
 
     @PostMapping(value = "/product", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "Fill the vending machine with products. The products are: OREO, MORENI, and DEYVIN.")
-    public List<Product> insertProducts(@RequestBody List<Product> products) {
-        this.shelves.products.addAll(products);
+    public List<Product> insertProducts(@RequestBody List<Product> products) throws Exception {
+
+        for (Product product : products) {
+            Integer occurrences = Collections.frequency(this.shelves.products, product);
+            final Integer LIMIT = 10;
+            if (occurrences < LIMIT) {
+                this.shelves.products.add(product);
+            } else {
+                throw new Exception("The machine can hold at most 10 " + product + "s.");
+            }
+        }
+
         return this.shelves.products;
     }
 
